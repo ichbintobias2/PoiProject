79b6413bd3675203971782d55e91850eded8c3b3
ichbintobias2 <t-gaming242@protonmail.com>
2021-07-18 17:07:50 +0200

    displaying POI from hardcoded addresses on map

--- index.js
+++ index.js
@@ -6,8 +6,34 @@ var mapDefaultZoom = 20;
 
 /* the main method of the program */
 function init_program() {
+    get_location_from_address("detmolder straße", "1", "bielefeld");
+    get_location_from_address("detmolder straße", "2", "bielefeld");
+    get_location_from_address("detmolder straße", "3", "bielefeld");
+
     initialize_map(-33.8688, 151.2093);
-    add_map_point(-33.8688, 151.2093);
+}
+
+function get_location_from_address(street, nr, city) {
+    var data = doRequest(street, nr, city);
+}
+
+function doRequest(street, nr, city) {
+    const url = "http://nominatim.openstreetmap.org/search?q="+ nr +"+"+ street +"+"+ city +"&format=json&polygon=1&addressdetails=1";
+    const Http = new XMLHttpRequest();
+
+    Http.open("GET", url);
+    Http.send();
+
+    Http.onreadystatechange = (e) => {
+        // only do something if the response is not empty
+        if (Http.responseText) {
+            console.log(Http.responseText);
+            var parsedData = JSON.parse(Http.responseText);
+            const lat = parsedData[0].lat;
+            const lng = parsedData[0].lon;
+            add_map_point(lat, lng);
+        }
+    }
 }
 
 function initialize_map(mapLat, mapLng) {
